e456979ba86596396bf572b3bc5c4896eed4f736
Kamil2601 <mich.kamil2601@gmail.com>
2021-08-11 13:44:59 +0200

    Move portal mechanic to separate script

--- Pacman/Assets/Scripts/MovingObjects/Player.cs
+++ Pacman/Assets/Scripts/MovingObjects/Player.cs
@@ -5,13 +5,11 @@ using UnityEngine;
 
 public class Player : MovingObject
 {
-    [SerializeField] private GameObject leftPortal;
-    [SerializeField] private GameObject rightPortal;
     private Quaternion rightRotation = Quaternion.Euler(0,0,0);
     private Quaternion leftRotation = Quaternion.Euler(0,0,180);
     private Quaternion upRotation = Quaternion.Euler(0,0,90);
     private Quaternion downRotation = Quaternion.Euler(0,0,270);
-    
+
     protected override void SetNextDirection()
     {
         int horizontal = (int)Input.GetAxisRaw("Horizontal");
@@ -37,17 +35,5 @@ public class Player : MovingObject
             transform.rotation = leftRotation;
         else if (currentDirection == Vector2.right)
             transform.rotation = rightRotation;
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject == leftPortal && currentDirection == Vector2.left)
-        {
-            transform.position = rightPortal.transform.position;
-        }
-        else if (other.gameObject == rightPortal && currentDirection == Vector2.right)
-        {
-            transform.position = leftPortal.transform.position;
-        }
     }
 }
--- Pacman/Assets/Scripts/Portal.cs
+++ Pacman/Assets/Scripts/Portal.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject output;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        other.transform.position = output.transform.position;
     }
 }
